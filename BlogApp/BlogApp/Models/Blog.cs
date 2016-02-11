7448f4b812ff8f46dc1d23e948dbe4e253fd0cd8
rominanayak <rominanayak30@gmail.com>
2016-02-11 16:27:11 -0500

    Adding models for blog and comment (Code First Method)

---
+++ BlogApp/BlogApp/Models/Blog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace bloggingPortal.Models
+{
+    public class Blog
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime PostedOn { get; set; }
+
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        public Blog()
+        {
+            PostedOn = DateTime.Now;
+        }
+    }
+}
---
+++ BlogApp/BlogApp/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace bloggingPortal.Models
+{
+    public class Comment
+    {
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Blog blog { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public Comment()
+        {
+            CreatedOn = DateTime.Now;
+        }
+    }
+}
